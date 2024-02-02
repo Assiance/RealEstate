e51693cfad36ec10e81ba500acd404d716b9e270
John Jones <jajones0622@gmail.com>
2024-02-01 19:45:29 -0500

    update the server.js to repo

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const app = express()
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const {connectToDb} = require('./db/data-connections')
-const {routes} = require('./routes/property-route')
+const {properyRoutes} = require('./routes/property-route')
+const {agentRoutes} = require('./routes/agent-route')
+const {companyRoutes} = require('./routes/company-route')
+const {userRoutes} = require('./routes/user-route')
+
 
 app.use(bodyParser.json())
-app.use('/',routes)
+app.use('/', properyRoutes)
+app.use('/', agentRoutes)
+app.use('/', companyRoutes)
+app.use('/', userRoutes)
+
 
 const PORT = 4000;
 app.listen(PORT, async () => {
