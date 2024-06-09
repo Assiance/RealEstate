a6d712d430ce48094ca7ca6fc6060f036d0fb0cb
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-06-09 19:34:56 -0400

    Update server.js
    Signed-off-by: Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/companies', companyRoutes)
 app.use('/users', userRoutes)
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`)
   await connectToDb();
